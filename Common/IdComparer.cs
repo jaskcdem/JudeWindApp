80bb0922bf56be041ecd03609d3ab3fca8743f49
杜沛鴻(jason_tu) <jason_tu@kunyoutech.com.tw>
2024-10-30 13:20:18 +0800

    add: comparer

---
+++ Common/IdComparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Common
+{
+    /// <summary> Id Comparer </summary>
+    public class IdComparer : IComparer<int?>
+    {
+        /// <summary> Id Compare </summary>
+        /// <remarks>null is smallest, than sort number desc </remarks>
+        public int Compare([AllowNull] int? x, [AllowNull] int? y)
+        {
+            // when x smaller than y, return -1
+            // when x same to y, return 0
+            // when x bigger than y, return 1
+            if (!x.HasValue && !y.HasValue) return 0;
+            else if (!x.HasValue && y.HasValue) return -1;
+            else if (!y.HasValue && x.HasValue) return 1;
+
+            int _compare = x!.Value.CompareTo(y!.Value);
+            return -_compare;
+        }
+    }
+}
---
+++ JudeWindApp/Controllers/SelfResearchController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JudeWindApp.Controllers
+{
+    /// <summary> something note </summary>
+    public class SelfResearchController : Controller
+    {
+    /// <summary> <see cref="Path.Combine(string, string)"/> </summary>
+        public List<string> PathCombine()
+        {
+            List<string> _result = [];
+            const string path1 = "c:\\temp\\", path2 = "\\upp", path3 = "\\upp\\", path4 = "\\upp\\123.txt", path5 = "upp\\123.txt"
+              , path6 = "upp", path7 = "upp\\";
+            _result.Add($"path1 + path2 = {Path.Combine(path1, path2)}");
+            _result.Add($"path1 + path3 = {Path.Combine(path1, path3)}");
+            _result.Add($"path1 + path4 = {Path.Combine(path1, path4)}");
+            _result.Add($"path1 + path5 = {Path.Combine(path1, path5)}");
+            _result.Add($"path1 + path6 = {Path.Combine(path1, path6)}");
+            _result.Add($"path1 + path7 = {Path.Combine(path1, path7)}");
+            return _result;
+        }
+    }
+}
