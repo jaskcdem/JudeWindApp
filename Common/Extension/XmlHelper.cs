cf456ce7c5090f4fe105aeeb4af09f7ed6fc2eaa
Tu, Jason <Jason.Tu@acer.com>
2025-07-09 14:17:36 +0800

    remove unknow namespace

--- Common/Extension/XmlHelper.cs
+++ Common/Extension/XmlHelper.cs
@@ -1,11 +1,6 @@
-﻿using Common.Paras;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
 using System.Runtime.Serialization.Json;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Schema;
